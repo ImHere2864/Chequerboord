fd85681ad7e5fb37314c06d3d72aa9e4ed1c27a6
ImHere2864 <samuel.novodomsky@spsehalova.sk>
2026-09-20 11:03:54 +0200

    Base for the checkerboard thingamajig

---
+++ chequerboord.ts
@@ -0,0 +1,17 @@
+const SIZE = 8;
+
+function fieldColor(row:number, column;number):string {
+    //TODO
+    return "";
+}
+
+function fieldSize(size:number):string[][] {
+    //TODO
+    return [];
+}
+
+function createBoard(board:string[][]):void {
+    //TODO
+}
+
+createBoard(fieldSize(SIZE))
